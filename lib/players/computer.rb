bd105168493b9b8b94589c2c414ab6b9035ea956
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-01-04 16:38:11 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -5,8 +5,6 @@ module Players
     end
 
     def logic(board)
-      x = 0
-      o = 0
       sides = [1,3,5,7]
       corners = [0,2,6,8]
       cpu = self.token
@@ -15,30 +13,27 @@ module Players
       Game::WIN_COMBINATIONS.each do |combo|
         a = combo.select{|x| board.cells[x] == cpu}
         b = combo.select{|x| board.cells[x] == opp}
-        if a.count(cpu) == 2 && b.count(opp) == 0
+        if !board.taken?(5)
+          return 5
+        elsif a.count(cpu) == 2 && b.count(opp) == 0
           c = combo.detect{|x| board.cells[x] == " "}
           return c + 1
         elsif a.count(cpu) == 0 && b.count(opp) == 2
           c = combo.detect{|x| board.cells[x] == " "}
-          return c + 1 
-        combo.each do |i|
-          if board.valid_move?(5)
-            return 5
-          elsif x == 2 && board.cells[i] == " "
-            return i + 1
-          elsif o == 2 && board.cells[i] == " "
-            return i + 1
-          elsif board.cells[0] == opp && board.cells[8] == opp
-            foo = sides.detect{|s| board.cells[s] == " "}
-            return foo + 1
-          elsif board.cells[2] == opp && board.cells[6] == opp
-            bar = sides.detect{|s| board.cells[s] == " "}
-            return bar + 1
-          else
-            c = corners.detect{|c| board.cells[c] == " "}
-            return c + 1 if c != nil
+          return c + 1
+        else
+          combo.each do |i|
+            elsif board.cells[0] == opp && board.cells[8] == opp
+              foo = sides.detect{|s| board.cells[s] == " "}
+              return foo + 1
+            elsif board.cells[2] == opp && board.cells[6] == opp
+              bar = sides.detect{|s| board.cells[s] == " "}
+              return bar + 1
+            else
+              c = corners.detect{|c| board.cells[c] == " "}
+              return c + 1 if c != nil
+            end
           end
-        end
       end
     end
 
