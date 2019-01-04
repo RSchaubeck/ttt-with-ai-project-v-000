95ee685184c58d912881569d446b36a97928da98
Richard Schaubeck <richard.schaubeck@gmail.com>
2019-01-04 17:59:33 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,11 +2,11 @@ module Players
   class Computer < Player
     def move(board)
       if start(board)
-        return start(board)
+        start(board)
       elsif block_or_win(board)
-        return block_or_win(board)
+        block_or_win(board)
       else
-        return corners_and_sides(board)
+        corners_and_sides(board)
       end
     end
 
